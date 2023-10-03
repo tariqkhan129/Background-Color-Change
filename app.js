498f47757d1c2c443c10d2964c6de28d0b1c729f
Tariq Khan <tariqcs129@gmail.com>
2023-10-02 17:26:34 -0700

    Background change

---
+++ app.js
@@ -0,0 +1,17 @@
+var bgChange = document.getElementById("bgChange");
+
+function blue() {
+    bgChange.style.backgroundColor = "blue";
+}
+function red() {
+    bgChange.style.backgroundColor = "red";
+}
+function yellow() {
+    bgChange.style.backgroundColor = "yellow";
+  }
+  function green() {
+    bgChange.style.backgroundColor = "green";
+  }
+  function black() {
+    bgChange.style.backgroundColor = "black";
+  }
